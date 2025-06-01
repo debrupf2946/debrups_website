5691b95b4516161959bcedd027faf09418d66593
debrupf2946 <f20212946@goa.bits-pilani.ac.in>
2025-06-02 02:25:23 +0530

    Updates

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  siteTitle: "Debrup's Website",
-  siteUrl: 'https://debrup.dev',
-  siteLogo: '/logo.png',
-  description:
-    'Software engineer and open source creator. This is my digital garden.',
-}
-
-export default config
